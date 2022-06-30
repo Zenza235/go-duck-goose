89551705315d4237732cd0f735ae60b9c7ae9b38
Zenza235 <paoloatienza235@gmail.com>
2022-06-29 21:10:05 -0400

    make player stop sliding on slope; finished basic movement

--- player/Player.gd
+++ player/Player.gd
@@ -1,13 +1,19 @@
 extends KinematicBody2D
 
+# code is currently temp; just to test movement
+# possible issue w/ speed being inconsistent on slopes
+
 var mov_spd = 300
 var jmp_spd = 300
 var gravity = 1000
 
 var accel = 0.1
-var friction = 0.1
+var friction = 0.05
 
 var velocity = Vector2.ZERO
+var is_jumping = false
+
+var snap
 
 func get_input():
 	var input_dir = 0
@@ -18,12 +24,19 @@ func get_input():
 	if input_dir != 0:
 		velocity.x = lerp(velocity.x, input_dir * mov_spd, accel)
 	else:
-		velocity.x = lerp(velocity.x, 0, friction)
+		if is_on_floor():
+			velocity.x = 0
+		else:
+			velocity.x = lerp(velocity.x, 0, friction)
 
 func _physics_process(delta):
 	get_input()
 	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity, Vector2.UP)
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
+	if is_on_floor():
+		is_jumping = false
+		if Input.is_action_just_pressed("jump"):
+			is_jumping = true
 			velocity.y = -jmp_spd
+	snap = transform.y * 8 if !is_jumping else Vector2.ZERO
+	velocity = move_and_slide_with_snap(velocity.rotated(rotation), snap, -transform.y, true)
+	velocity = velocity.rotated(-rotation)
