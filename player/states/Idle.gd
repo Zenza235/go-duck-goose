be5246e5851fa702bada190040a4ddd8df3ebdfd
Zenza235 <paoloatienza235@gmail.com>
2022-07-12 17:13:53 -0400

    update transitions

--- player/states/Idle.gd
+++ player/states/Idle.gd
@@ -1,14 +1,14 @@
 extends PlayerState
 
-func enter(msg := {}) -> void:
+func enter() -> void:
     player.velocity = Vector2.ZERO
 
 func update(delta: float) -> void:
     if not player.is_on_floor():
-        state_machine.transition_to("Air"):
+        state_machine.transition_to("Fall"):
 
     if Input.is_action_just_pressed("jump"):
-        state_machine.transition_to("Air", {do_jump = true})
+        state_machine.transition_to("Jump")
     
     if Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("move_right"):
         state_machine.transition_to("Run")
--- player/states/Run.gd
+++ player/states/Run.gd
@@ -2,7 +2,7 @@ extends PlayerState
 
 func physics_update(delta: float) -> void:
     if not player.is_on_floor():
-        state_machine.transition_to("Air"):
+        state_machine.transition_to("Fall"):
     
     var move_dir: float = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
     player.velocity.x = player.move_spd * move_dir
@@ -13,7 +13,7 @@ func physics_update(delta: float) -> void:
     player.velocity = player.velocity.rotated(-player.rotation)
 
     if Input.is_action_just_pressed("jump"):
-        state_machine.transition_to("Air", {do_jump = true})
+        state_machine.transition_to("Jump")
     
     elif is_equal_approx(move_dir, 0.0):
         state_machine.transition_to("Idle")
