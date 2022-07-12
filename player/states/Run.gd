842ea1618fe290a23ff828ee1fc7c88c122a55cb
Zenza235 <paoloatienza235@gmail.com>
2022-07-12 16:43:49 -0400

    implement run state

---
+++ player/states/Run.gd
@@ -0,0 +1,19 @@
+extends PlayerState
+
+func physics_update(delta: float) -> void:
+    if not player.is_on_floor():
+        state_machine.transition_to("Air"):
+    
+    var move_dir: float = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+    player.velocity.x = player.move_spd * move_dir
+    player.velocity.y = player.GRAVITY * delta
+
+    snap_vec = player.transform.y * 8
+    player.velocity = move_and_slide_with_snap(player.velocity.rotated(player.rotation), snap_vec, -player.transform.y, true)
+    player.velocity = player.velocity.rotated(-player.rotation)
+
+    if Input.is_action_just_pressed("jump"):
+        state_machine.transition_to("Air", {do_jump = true})
+    
+    elif is_equal_approx(move_dir, 0.0):
+        state_machine.transition_to("Idle")
